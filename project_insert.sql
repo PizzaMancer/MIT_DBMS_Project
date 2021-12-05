5937d009eaae906f4519d6da44643984fee14ef7
Akshay Dhansoia <dhansoia.akshay@gmail.com>
2021-12-05 20:11:12 +0530

    Add files via upload

---
+++ project_insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO CUSTOMER VALUES(412,'energy.synergist@gmail.comEnergy Synergist','Immadihalli Rd, Whitefield','Karnataka','Bangalore',560066,'SRRxK5xz','PAID');
+INSERT INTO CUSTOMER VALUES(873,'z.n@gmail.comZubin Nibhanupudi','28-C, Huseini Bldg, Kalbadevi Road, Kalbadevi','Maharashtra','Mumbai',400615,'mTKuN8xY','UNPAID');
+INSERT INTO CUSTOMER VALUES(393,'shahrukh.ahmad@gmail.comShahrukh Ahmad','B-116, Avinash Building, Versova Rd, Andheri','Maharashtra','Mumbai',400061,'Dbi66l9P','UNPAID');
+INSERT INTO CUSTOMER VALUES(68,'infrared.investment@gmail.comInfrared Investment','54b Hazra Road, Kalighat','West Bengal','Kolkata',700001,'AeOWja4S','PAID');
+INSERT INTO CUSTOMER VALUES(250,'abhilasha.ahmad@gmail.comAbhilasha Ahmad','Shop No 11-175/14/a, Fathenagar','Telangana','Hyderabad',500033,'2ekLECZO','PAID');
+INSERT INTO CUSTOMER VALUES(133,'pasha.power@gmail.comPasha Power Corp','18 Cathedral Road, Gopalapuram','Tamil Nadu','Chennai',600020,'AHYKm8lQ','UNPAID');
+INSERT INTO CUSTOMER VALUES(169,'loopa.srivastav@gmail.comLoopa Srivastav','51 B, Main Pusa Road, Pusa Road','Delhi','Delhi',110006,'pyUthRCV','DEAD');
+INSERT INTO CUSTOMER VALUES(821,'ratna.patil@gmail.comRatna Patil','34-4-5534/39, Musheerabad','Telangana','Hyderabad',500032,'W0ttJm8k','PAID');
+INSERT INTO CUSTOMER VALUES(791,'surinder.kulkarni@gmail.comSurinder Kulkarni','Marghafarm Compound, Bapunagar','Gujarat','Ahmedabad',380009,'1IJ3Xqmw','UNPAID');
+INSERT INTO CUSTOMER VALUES(719,'phalanx.holding@gmail.comPhalanx Holding','S.no.27, Vanasthalipuram','Telangana','Hyderabad',500034,'qEPuV1oi','PAID');
+
+INSERT INTO ACCOUNT VALUES(9757, 1, 'Energy Synergist','Corporate','Immadihalli Rd, Whitefield, Bangalore','WN75LR84G7','SUBSIDISED');
+INSERT INTO ACCOUNT VALUES(7307, 3, 'Zubin Nibhanupudi','Domestic','28-C, Huseini Bldg, Kalbadevi Road, Kalbadevi, Mumbai','T0EX7PA9AZ','REGULAR');
+INSERT INTO ACCOUNT VALUES(8249, 2, 'Shahrukh Ahmad','Domestic','B-116, Avinash Building, Versova Rd, Andheri, Mumbai','NXFH63X72S','SUBSIDISED');
+INSERT INTO ACCOUNT VALUES(1156, 1, 'Infrared Investment','Corporate','54b Hazra Road, Kalighat, Kolkata','WAWOI8R4OZ','REGULAR');
+INSERT INTO ACCOUNT VALUES(5114, 2, 'Abhilasha Ahmad','Domestic','Shop No 11-175/14/a, Fathenagar, Hyderabad','WQ22TU1HZ5','REGULAR');
+INSERT INTO ACCOUNT VALUES(3221, 2, 'Pasha Power Corp','Corporate','18 Cathedral Road, Gopalapuram, Chennai','WPHME246PC','REGULAR');
+INSERT INTO ACCOUNT VALUES(4045, 3, 'Loopa Srivastav','Domestic','51 B, Main Pusa Road, Pusa Road, Delhi','N6FSRBLU35','SUBSIDISED');
+INSERT INTO ACCOUNT VALUES(7134, 1, 'Ratna Patil','Domestic','34-4-5534/39, Musheerabad, Hyderabad','AG60KKK6C3','REGULAR');
+INSERT INTO ACCOUNT VALUES(4168, 4, 'Surinder Kulkarni','Domestic','Marghafarm Compound, Bapunagar, Ahmedabad','NOPYO8HA5I','REGULAR');
+INSERT INTO ACCOUNT VALUES(7825, 2, 'Phalanx Holding','Corporate','S.no.27, Vanasthalipuram, Hyderabad','BW9XE543J5','REGULAR');
+
+INSERT INTO TARIFF VALUES(211, 'Domestic Tariff Charges' ,'Live');
+INSERT INTO TARIFF VALUES(247, 'Domestic Tariff Charges Metro' ,'Update');
+INSERT INTO TARIFF VALUES(154, 'Corporate Tariff Charges' ,'Live');
+INSERT INTO TARIFF VALUES(130, 'Corporate Tariff Charges Metro' ,'Live');
